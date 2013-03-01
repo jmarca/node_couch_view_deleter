832106560f05e27b50e6444147d97d1c2fda0563
James E. Marca <jmarca@translab.its.uci.edu>
2013-02-28 20:39:48 -0800

    seems to be running in practice, up the limit to 1000 from 10
    modified:   couch_view_deleter.js
    
    add optimist as dependency
    modified:   package.json

--- couch_view_deleter.js
+++ couch_view_deleter.js
@@ -6,15 +6,15 @@ var async = require('async');
 var optimist = require('optimist')
 var argv = optimist
            .usage('drop troublesome docs from couchdb, using a view.\nUsage: $0')
-           .options('d',{'required':true
+           .options('d',{'demand':'d'
                         ,'alias':'db'
                         ,'describe':'The database to query'
                         })
-           .options('v',{'required':true
+           .options('v',{'demand':'v'
                         ,'alias': 'view'
                         ,'describe':'The view to use.  Every doc that is pulled into the view will be deleted'
                         })
-           .options('dd',{'required':true
+           .options('dd',{'demand':'dd'
                         ,'alias': 'designdoc'
                         ,'describe':'The design doc that contains the view, above.'
                         })
@@ -30,13 +30,13 @@ if (argv.help){
     return null
 }
 
-var cvd = require('.').couch_view_deleter
+var cvd = require('./index').couch_view_deleter
 
 async.nextTick(function(){
     cvd({db:argv.d
         ,design:argv.dd
         ,view:argv.v
-        ,limit:10}
+        ,limit:1000}
        ,function(){
             console.log('all done')
             return null
