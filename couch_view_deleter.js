be4e2a58c8f539fbe7334b56d27e573a12a595a7
James E. Marca <james@activimetrics.com>
2015-12-28 22:46:40 -0800

    migrating to new usage

--- couch_view_deleter.js
+++ couch_view_deleter.js
@@ -1,50 +1,38 @@
 /* global JSON console require process */
 var request = require('request');
-var async = require('async');
 
+var argv = require('minimist')(process.argv.slice(2))
+var path = require('path')
 
-var optimist = require('optimist')
-var argv = optimist
-           .usage('drop troublesome docs from couchdb, using a view.\nUsage: $0')
-           .options('d',{'demand':'d'
-                        ,'alias':'db'
-                        ,'describe':'The database to query'
-                        })
-           .options('v',{'demand':'v'
-                        ,'alias': 'view'
-                        ,'describe':'The view to use.  Every doc that is pulled into the view will be deleted'
-                        })
-           .options('dd',{'demand':'dd'
-                        ,'alias': 'designdoc'
-                        ,'describe':'The design doc that contains the view, above.'
-                        })
-.options("l",{'alias':'limit'
-                         ,'describe': "how many docs at a time to process"
-                         ,'default': 1000
-           })
-           .options("h", {'alias':'help'
-                         ,'describe': "display this hopefully helpful message"
-                         ,'type': "boolean"
-                         ,'default': false
-           })
-           .argv
-;
-if (argv.help){
-    optimist.showHelp();
-    return null
+// configuration stuff
+var rootdir = path.normalize(__dirname)
+
+// where are the files
+
+var config_file
+if(argv.config === undefined){
+    config_file = path.normalize(rootdir+'/config.json')
+}else{
+    config_file = path.normalize(rootdir+'/'+argv.config)
 }
+console.log('setting configuration file to ',config_file,'.  Change with the --config option.')
 
-var cvd = require('./index').couch_view_deleter
 
-async.nextTick(function(){
-    cvd({db:argv.d
-        ,design:argv.dd
-        ,view:argv.v
-        ,limit:argv.l}
-       ,function(){
-            console.log('all done')
-            return null
-        })
-});
 
-1; // long live perl
+var config_okay = require('config_okay')
+
+
+var cvd = require('.').couch_view_deleter
+
+config_okay(config_file,function(e,c){
+    if(e || c === undefined){
+        console.log('a proper config file is required.  See the README for help')
+        throw new Error('no config file found or wrong permissions')
+    }
+    if(c.couchdb.view === undefined ) throw new Error('please define a proper view path in the config file under couchdb.view  See the README for help')
+    cvd(c.couchdb,function(){
+        console.log('all done')
+        return null
+    })
+    return null
+})
