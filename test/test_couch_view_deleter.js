1d3cfd8dd8b5ec7320524161661d75b2a367179c
James E. Marca <james@activimetrics.com>
2015-12-28 22:28:47 -0800

    modified to use newer ways to do things
    use config_okay, and also use queue-async, not async, and also some
    libraries like couchdb_get_views
    
    test revised and passes, deleting 1001 documents in two passes of
    limit=1000.

--- test/test_couch_view_deleter.js
+++ test/test_couch_view_deleter.js
@@ -7,14 +7,15 @@ var library = require('../.')
 var cvd = library.couch_view_deleter
 var request = require('request')
 var _ = require('lodash')
-var env = process.env;
-var cuser = env.COUCHDB_USER ;
-var cpass = env.COUCHDB_PASS ;
-var chost = env.COUCHDB_HOST || 'localhost';
-var cport = env.COUCHDB_PORT || 5984;
 
-var test_db ='test%2fview%2fdeleter'
-var couch = 'http://'+chost+':'+cport+'/'+test_db
+var path    = require('path')
+var rootdir = path.normalize(__dirname)
+var config_file = rootdir+'/../test.config.json'
+
+
+var config_okay = require('config_okay')
+
+var test_db = 'test%2fview%2fdeleter' // default value
 
 var docs = {'docs':[{foo:'bar'}
                    ,{'baz':'bat'}
@@ -22,74 +23,134 @@ var docs = {'docs':[{foo:'bar'}
                    ,{'baz':'bat','ts':new Date()}
                    ,{"_id":"_design/test","language":"javascript","views":{"testview":{"map":"function(doc) {\n  if(doc.ts !== undefined){\n    if(doc.data === undefined){\n      emit(null, 1)\n  }}}"}}}]}
 
-describe('view deletion',function(){
-    var created_locally=false
-    before(function(done){
-        // create a test db, the put data into it
-        var opts = {'uri':couch
-                   ,'method': "PUT"
-                   ,'headers': {}
-                   };
-        opts.headers.authorization = 'Basic ' + new Buffer(cuser + ':' + cpass).toString('base64')
-        opts.headers['Content-Type'] =  'application/json'
-        request(opts
-               ,function(e,r,b){
-                    if(!e)
-                        created_locally=true
-                    // now populate that db with some docs
-                    opts.method='POST'
-                    opts.json=docs
-                    opts.uri += '/_bulk_docs'
-                    request(opts,function(e,r,b){
-                        if(e) done(e)
-                        docs=[]
-                        _.each(b
-                              ,function(resp){
-                                   resp.should.have.property('ok')
-                                   docs.push({_id:resp.id
-                                             ,_rev:resp.rev})
-                               });
-                        return done()
-                    })
-                    return null
-                })
-        return null
-    })
-    after(function(done){
-        if(!created_locally) return done()
+// add 1000 more test docs without valid data
+for(var i = 0 ; i<1000; i++){
+    docs.docs.push({foo:'bar','ts':new Date()})
+}
 
-        var couch = 'http://'+chost+':'+cport+'/'+test_db
-        // bail in development
-        //console.log(couch)
-        //return done()
-        var opts = {'uri':couch
-                   ,'method': "DELETE"
-                   ,'headers': {}
-                   };
-        opts.headers.authorization = 'Basic ' + new Buffer(cuser + ':' + cpass).toString('base64')
-        opts.headers['Content-Type'] =  'application/json'
-        request(opts
-               ,function(e,r,b){
-                    if(e) return done(e)
-                    return done()
+var config={}
+
+function create_tempdb(cb){
+    var date = new Date()
+    var test_db_unique = [config.couchdb.db,
+                          date.getHours(),
+                          date.getMinutes(),
+                          date.getSeconds(),
+                          date.getMilliseconds()].join('-')
+    config.couchdb.db = test_db_unique
+    var cdb =
+        [config.couchdb.host+':'+config.couchdb.port
+        ,config.couchdb.db].join('/')
+
+    if(! /http/.test(cdb)){
+        cdb = 'http://'+cdb
+    }
+    var opts = {'uri':cdb
+                ,'method': "PUT"
+                ,'headers': {}
+               };
+    var cuser = config.couchdb.auth.username
+    var cpass = config.couchdb.auth.password
+
+    opts.headers.authorization = 'Basic ' + new Buffer(cuser + ':' + cpass).toString('base64')
+    opts.headers['Content-Type'] =  'application/json'
+
+    request(opts
+            ,function(e,r,b){
+                if(e) return cb(e)
+                var c = JSON.parse(b)
+                if(c.error ){
+                    console.log(e,c)
+                    return cb('error creating db')
+                }
+                // now populate that db with some docs
+                opts.method='POST'
+                opts.json=docs
+                opts.uri += '/_bulk_docs'
+                request(opts,function(e,r,b){
+                    if(e) return cb(e)
+                    docs=[]
+                    _.each(b
+                           ,function(resp){
+                               resp.should.have.property('ok')
+                               docs.push({_id:resp.id
+                                          ,_rev:resp.rev})
+                           });
+                    return cb()
                 })
+                return null
+    })
+    return null
+}
+
+function delete_tempdb(cb){
+    var cdb =
+        [config.couchdb.host+':'+config.couchdb.port
+        ,config.couchdb.db].join('/')
+
+    if(! /http/.test(cdb)){
+        cdb = 'http://'+cdb
+    }
+    var opts = {'uri':cdb
+                ,'method': "DELETE"
+                ,'headers': {}
+               };
+    var cuser = config.couchdb.auth.username
+    var cpass = config.couchdb.auth.password
+
+    opts.headers.authorization = 'Basic ' + new Buffer(cuser + ':' + cpass).toString('base64')
+    opts.headers['Content-Type'] =  'application/json'
+
+    request(opts
+            ,function(e,r,b){
+                if(e) return cb(e)
+                var c = JSON.parse(b)
+                if(c.error ){
+                    console.log(e,c)
+                    console.log('manually delete',cdb)
+                }
+                return cb()
+
+    })
+    return null
+}
+
+before(function(done){
+    config_okay(config_file,function(err,c){
+        if(!c.couchdb.db){ throw new Error('need valid db defined in test.config.json')}
+        config = c
+        create_tempdb(done)
         return null
     })
+    return null
+})
+after(delete_tempdb)
+
+describe('view deletion',function(){
+
     it('should work',function(done){
-        var bd = cvd({'db':test_db
-                     ,'design':'test'
-                     ,'view':'testview'}
-                    ,function(e){
-                         if(e) return done(e)
-                         request.get(couch+'/_all_docs?include_docs=true'
-                                           ,function(e,r,b){
-                                                if(e) return done(e)
-                                                var c=JSON.parse(b)
-                                                c.should.have.property('total_rows',4) // one less than I started with
-                                                return done()
-                                            })
-                         return null
-                     })
+        config.couchdb.view='_design/test/_view/testview'
+        var bd = cvd(config.couchdb
+                     ,function(e){
+                        if(e) return done(e)
+                        var cdb =
+                                [config.couchdb.host+':'+config.couchdb.port
+                                 ,config.couchdb.db].join('/')
+
+                         if(! /http/.test(cdb)){
+                             cdb = 'http://'+cdb
+                         }
+                        request.get(cdb+'/_all_docs?include_docs=true'
+                                    ,function(e,r,b){
+                                        if(e) return done(e)
+                                        var c=JSON.parse(b)
+                                        c.should.have.property('total_rows')
+                                        var tr = c.total_rows
+                                        tr.should.eql(4) // one less than I started with
+                                        return done()
+                                    })
+                        return null
+                    })
         return null
     })
 
